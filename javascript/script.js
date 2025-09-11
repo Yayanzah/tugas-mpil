3cf481cb359de87771a415e41ac3d36fba028484
yayan <githabaja@gmail.com>
2025-09-11 09:34:57 +0700

    tambahkan script.js untuk menghitung panjang nama dan menampilkan hasilnya.

---
+++ javascript/script.js
@@ -0,0 +1,7 @@
+let nama = "Yayan";
+let jumlahHuruf = nama.length;
+let hasil = jumlahHuruf * 5;
+
+console.log("Nama:", nama);
+console.log("Jumlah huruf:", jumlahHuruf);
+console.log("Hasil (jumlah huruf x 5):", hasil);
