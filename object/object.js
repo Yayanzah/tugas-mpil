81f189be38704177578d2fb9ea6af9b0f4884053
yayan <githabaja@gmail.com>
2025-09-11 18:52:09 +0700

    menambahkan objek student dengan metode sayHello dan informasi terkait

---
+++ object/object.js
@@ -0,0 +1,12 @@
+let student = {
+  nama: "Yayan",
+  umur: 17,
+  hobby: "mancing",
+  sayHello: function () {
+    return "Halo, nama saya " + this.nama;
+  },
+};
+
+console.log(student.sayHello()); // Halo, nama saya Yayan
+console.log("Umur saya adalah " + student.umur + " tahun."); // Umur saya adalah 19 tahun.
+console.log("Hobby saya adalah " + student.hobby + "."); // Hobby saya adalah mancing.
